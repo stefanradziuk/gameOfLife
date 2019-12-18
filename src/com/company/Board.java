a5817aa45fe4bc5939a9b9b809fbd27ba35d42d0
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2019-12-18 20:46:57 +0000

    debugging neighbours

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -3,9 +3,13 @@ package com.company;
 public class Board {
 
   private State[][] board;
+  private int rows;
+  private int cols;
 
   public Board(int rows, int cols) {
     board = new State[rows][cols];
+    this.rows = rows;
+    this.cols = cols;
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
         board[i][j] = State.DEAD;
@@ -24,9 +28,45 @@ public class Board {
   public void printBoard() {
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
-        System.out.print(board[i][j].toString());
+        System.out.print(getPoint(i, j).toString());
       }
       System.out.print('\n');
     }
+  }
+
+  public int neighbours(int row, int col) {
+    int result = 0;
+    for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
+        int nRow = row + i;
+        int nCol = col + j;
+        if (i * j != 0 &&
+            nRow >= 0 && nRow < this.rows &&
+            nCol >= 0 && nCol < this.cols) {
+          switch (getPoint(nRow, nCol)) {
+            case ALIVE:
+              result += 1;
+            case DEAD:
+              break;
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  public Board nextGen() {
+    Board nextGen = new Board(rows, cols);
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        int neighbours = this.neighbours(i, j);
+        if (neighbours == 3) {
+          nextGen.setPoint(i, j, State.ALIVE);
+        } else if (neighbours == 2 && this.getPoint(i, j).equals(State.ALIVE)) {
+          nextGen.setPoint(i, j, State.ALIVE);
+        }
+      }
+    }
+    return nextGen;
   }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,21 @@ public class Main {
 
   public static void main(String[] args) {
     Board board1 = new Board(10, 10);
-    board1.setPoint(3, 1, State.ALIVE);
-    board1.setPoint(4, 1, State.ALIVE);
-    board1.setPoint(6, 1, State.ALIVE);
+    board1.setPoint(3, 3, State.ALIVE);
+    board1.setPoint(4, 3, State.ALIVE);
+    board1.setPoint(5, 3, State.ALIVE);
+
+    System.out.println(board1.neighbours(4, 2));
 
     board1.printBoard();
+    System.out.println("------------");
+    board1.nextGen().printBoard();
+    System.out.println("------------");
+    board1.nextGen().nextGen().printBoard();
+    System.out.println("------------");
+    board1.nextGen().nextGen().nextGen().printBoard();
+    System.out.println("------------");
+    board1.nextGen().nextGen().nextGen().nextGen().printBoard();
+
   }
 }
